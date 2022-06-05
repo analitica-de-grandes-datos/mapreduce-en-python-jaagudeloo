83e2f6a225897bb92346e0a7592a297bf74344f0
jaagudeloo <jaagudeloo@unal.edu.co>
2022-06-05 16:50:57 -0500

    Lab2_v0
    Lab2_v0

--- pregunta_02/mapper.py
+++ pregunta_02/mapper.py
@@ -1,3 +1,12 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+    tabla = [line.replace("\n", "") for line in sys.stdin]
+    tabla = [line.split(",") for line in tabla]
+    col_3 = [tabla[i][3] for i in range(len(tabla))]
+    col_4 = [tabla[i][4] for i in range(len(tabla))]
+
+    for line in range(len(tabla)):
+        sys.stdout.write("{}\t{}\n".format(col_3[line],col_4[line]))
--- pregunta_02/reducer.py
+++ pregunta_02/reducer.py
@@ -1,3 +1,31 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == '__main__':
+
+    curkey = None
+    contador = 0
+
+    for line in sys.stdin:
+
+        key, val = line.split("\t")
+        val = int(val)
+
+        if key == curkey:
+            if contador >= val:
+                contador = contador
+            else:
+                contador = val
+        else:
+
+            if curkey is not None:
+
+                sys.stdout.write("{}\t{}\n".format(curkey, contador))
+                contador = 0
+
+            curkey = key
+            contador = val
+
+    sys.stdout.write("{}\t{}\n".format(curkey, contador))
