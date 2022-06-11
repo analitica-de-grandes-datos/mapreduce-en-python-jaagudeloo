99460b6e0ae86251034566ae6fe5bf361fcdb8ea
jaagudeloo <jaagudeloo@unal.edu.co>
2022-06-11 17:20:18 -0500

    Lab2_v6
    Lab2_v6

--- pregunta_10/reducer.py
+++ pregunta_10/reducer.py
@@ -1,3 +1,27 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+from collections import defaultdict
+import operator
+
+if __name__ == '__main__':
+
+    diccio = defaultdict(list)
+
+    for line in sys.stdin:
+
+        numero, val = line.replace("\n","").strip().split("	")
+        val = val.split(",")
+        numero = int(numero)
+        for elemento in val:
+            diccio[elemento].append(numero)
+        #print(val)
+
+    diccio_ord = sorted(diccio.items(), key = operator.itemgetter(0))
+    #print(diccio_ord)
+
+    for clave, valor in diccio_ord:
+        datos_num = sorted(valor, key = int)
+        valor = ",".join(map(str, datos_num))
+        sys.stdout.write("{}    {}\n".format(clave, valor))
