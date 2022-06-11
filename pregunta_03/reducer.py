242bd7d22e92825d86f9504bb447e077031db61c
jaagudeloo <jaagudeloo@unal.edu.co>
2022-06-11 13:38:00 -0500

    Lab2_v1
    Lab2_v1

--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -2,27 +2,19 @@
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
 import sys
+import operator
 
 if __name__ == '__main__':
 
-    curkey = None
-    total = 0
+    diccio = {}
 
     for line in sys.stdin:
 
         key, val = line.split("\t")
         val = int(val)
+        diccio[key] = val
 
-        if key == curkey:
+    diccio_ord = sorted(diccio.items(), key = operator.itemgetter(1))
 
-            total += val
-        else:
-
-            if curkey is not None:
-
-                sys.stdout.write("{}\t{}\n".format(curkey, total))
-
-            curkey = key
-            total = val
-
-    sys.stdout.write("{}\t{}\n".format(curkey, total))
+    for campo in diccio_ord:
+        sys.stdout.write("{},{}\n".format(campo[0], campo[1]))
--- pregunta_04/mapper.py
+++ pregunta_04/mapper.py
@@ -1,3 +1,11 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+    tabla = [line.replace("\n", "") for line in sys.stdin]
+    tabla = [line.split("   ") for line in tabla]
+    col_0 = [tabla[i][0] for i in range(len(tabla))]
+
+    for line in col_0:
+        sys.stdout.write("{}\t1\n".format(line))
