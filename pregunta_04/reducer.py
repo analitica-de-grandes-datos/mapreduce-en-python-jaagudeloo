e9d7f477b8b0022e2540adbe57a7ec8cb5d01bf7
jaagudeloo <jaagudeloo@unal.edu.co>
2022-06-11 13:53:39 -0500

    Lab2_v2
    Lab2_v2

--- pregunta_04/reducer.py
+++ pregunta_04/reducer.py
@@ -20,9 +20,9 @@ if __name__ == '__main__':
 
             if curkey is not None:
 
-                sys.stdout.write("{}\t{}\n".format(curkey, total))
+                sys.stdout.write("{},{}\n".format(curkey, total))
 
             curkey = key
             total = val
 
-    sys.stdout.write("{}\t{}\n".format(curkey, total))
+    sys.stdout.write("{},{}\n".format(curkey, total))
--- pregunta_05/mapper.py
+++ pregunta_05/mapper.py
@@ -1,3 +1,13 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+    tabla = [line.replace("\n", "") for line in sys.stdin]
+    tabla = [line.split("   ") for line in tabla]
+    col_1 = [tabla[i][1] for i in range(len(tabla))]
+    col_1 = [line.split("-") for line in col_1]
+    col_1_1 = [col_1[i][1] for i in range(len(col_1))]
+
+    for line in col_1_1:
+        sys.stdout.write("{}\t1\n".format(line))
